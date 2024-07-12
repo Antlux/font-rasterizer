b67c034a295a43ed158bb92a94e4ae280a65aff2
Antlux <gurlux@icloud.com>
2024-07-12 12:48:20 +0200

    Environment argument handling
    Environment argument handling for program subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = args();
+    if let Some(subcommand) = args.nth(1) {
+        match subcommand.as_str() {
+            other => {
+                println!("'{other}' is not a recognized subcommand.");
+                help();
+            }         
+        }
+    }
+}
+
+fn help() {
+    println!("===== HELP =====");
+    println!("subcommands:");
+    println!("  > nothing")
 }
